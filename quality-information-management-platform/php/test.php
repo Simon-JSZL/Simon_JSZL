f8e5d9e7a709afb71d802b4168f6b8dcbe94ff89
Simon-JSZL <39123509+Simon-JSZL@users.noreply.github.com>
2018-10-09 13:23:41 +0800

    add a new module,working on it

--- quality-information-management-platform/php/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('./CountingFail_Wagon.php');
-$CountingFailWagon=new CountingFailWagon();
-$TypFail = $CountingFailWagon->typfail('J5','0DZ314');
-$result = array("TypFail"=> $TypFail);
-print_r(($TypFail));
-echo json_encode($result);
