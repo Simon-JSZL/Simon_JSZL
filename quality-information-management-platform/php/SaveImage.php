edae2bb1281ed553cc139475b374b3e14f37f86a
Simon-JSZL <39123509+Simon-JSZL@users.noreply.github.com>
2018-10-26 15:54:43 +0800

    add a new module,working on it

--- quality-information-management-platform/php/SaveImage.php
+++ quality-information-management-platform/php/SaveImage.php
@@ -1,7 +1,8 @@
 <?php
+
 function IpAddress(){
-    //$MachineId=trim($_GET['MachineId']);
-    $MachineId="J5";
+    $MachineId=trim($_POST['MachineId']);
+    //$MachineId="J5";
     switch ($MachineId){
         //case "J5":$IpAddress="10.17.57.39";break;
         //case "J6":$IpAddress="10.17.57.37";break;
@@ -15,8 +16,8 @@ function IpAddress(){
     return $IpAddress;
 }
 function WagonName(){
-    //$WagonName=$_GET['WagonName'];
-    $WagonName="0DZ180";
+    $WagonName=$_POST['WagonName'];
+    //$WagonName="0DZ180";
     return $WagonName;
 }
 function Connect2Machine()
@@ -35,23 +36,81 @@ function Connect2Machine()
     else
         return $conn;
 }
+function create_zip($files = array(),$destination = '',$overwrite = false) {
+    //if the zip file already exists and overwrite is false, return false
+    if(file_exists($destination) && !$overwrite) { return false; }
+    if(count($files)) {
+        //create the archive
+        $zip = new ZipArchive();
+        if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+            return false;
+        }
+        //add the files
+        foreach($files as $file) {
+            $zip->addFile($file,$file);
+        }
+        //debug
+        //echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
+        $zip->close();
+        //check to make sure the file exists
+        header('Content-type: application/zip');
+        header('Content-Disposition: attachment; filename="'.WagonName().'.zip"');
+        readfile($destination);
+        return "下载完成";
+    }
+    else
+    {
+        return "下载失败";
+    }
+}
+function Delete($path)
+{
+    if (is_dir($path) === true)
+    {
+        $files = array_diff(scandir($path), array('.', '..'));
+
+        foreach ($files as $file)
+        {
+            Delete(realpath($path) . '/' . $file);
+        }
+
+        return rmdir($path);
+    }
+
+    else if (is_file($path) === true)
+    {
+        return unlink($path);
+    }
+
+    return false;
+}
 function ReturnData()
 {
-    $result=array();
-    $WagonName = 'T' . WagonName();
+    $WagonName =WagonName();
     $header="424d56010100000000003600000028000000b40000007800000001002000000000000000000000000000000000000000000000000000";
     $conn = Connect2Machine();
     $params = array();
     $options = array("Scrollable" => SQLSRV_CURSOR_KEYSET);
-    $sql = "select [Index] as Id,ErrorImage from " . $WagonName;
+    $sql = "select [Index] as Id,ErrorImage from " . "T" . $WagonName ;
     $query= sqlsrv_query($conn, $sql, $params, $options);
-    while($row= sqlsrv_fetch_array($query)){
-        $Image=base64_encode(hex2bin($header).$row['ErrorImage']);
-        $result[]=array(
-            "Id"=>$row['Id'],
-            "Image"=>$Image
-        );
+    @ $row_count = sqlsrv_num_rows($query);
+    if($row_count==0){
+        $result="未查询到该车次信息，请确认输入参数无误";
     }
-    return $result;
+    else {
+        mkdir("../".$WagonName);
+        define("IMAGE_FOLDER", "../".$WagonName."/");
+        $FileList=array();
+        while ($row = sqlsrv_fetch_array($query)) {
+            $Image = hex2bin($header) . $row['ErrorImage'];
+            $file = IMAGE_FOLDER . $row['Id'].'.jpg';
+            file_put_contents($file, $Image);
+            $FileList[]=$file;
+        }
+        $result=create_zip($FileList,IMAGE_FOLDER .$WagonName .'.zip');
+        Delete(IMAGE_FOLDER);
+    }
+return $result;
 }
-echo json_encode(ReturnData());
+
+echo ReturnData();
