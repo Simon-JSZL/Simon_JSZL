3dbdcb890570c89d8a7f75f1344bd3d7dd2fa0ba
Simon-JSZL <39123509+Simon-JSZL@users.noreply.github.com>
2019-03-14 22:34:48 +0800

    Adding new page

---
+++ quality-information-management-platform/php/ReturnProcedure.php
@@ -0,0 +1,9 @@
+<?php
+include('./ConnectInfo.php');
+    function returnProcedure($machineId){
+        $ConnInfo=new ConnectInfo();
+        $sql = "select * from dbo.MachineInfo where MachineId = '".$machineId."'";
+        $row= $ConnInfo->returnRow($sql);
+        $Info = array("MachineId"=>$machineId,"Procedure"=>$row['SideId'],"ProductId"=>$row['ProductId']);
+        return $Info;
+    }
